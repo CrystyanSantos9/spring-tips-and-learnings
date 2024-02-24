76f74b2dc56c191aa54f61292cda21aac752fe28
Crystyan S. Santos <47446514+CrystyanSantos9@users.noreply.github.com>
2024-02-24 14:55:56 -0300

    usando redis-sentinels

--- src/main/java/com/cryss/tipsandlearnings/config/RedisConfig.java
+++ src/main/java/com/cryss/tipsandlearnings/config/RedisConfig.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import io.lettuce.core.ReadFrom;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cache.Cache;
@@ -30,19 +31,10 @@ import org.springframework.cache.annotation.CachingConfigurer;
 
 import java.time.Duration;
 
+@Slf4j
 @Configuration
-@Log4j2
 @RequiredArgsConstructor
 public class RedisConfig implements CachingConfigurer {
-
-    @Value ("${spring.data.redis.sentinel.nodes}")
-    private String redisHost;
-
-
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-
-
 
     @Value("${spring.cache.redis.time-to-live}")
     private long redisTimeToLive;
@@ -50,38 +42,53 @@ public class RedisConfig implements CachingConfigurer {
     @Value("${spring.data.redis.timeout}")
     private Duration redisCommandTimeout;
 
+    private final RedisProperties redisProperties;
+
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(redisHost, redisPort);
+    protected LettuceConnectionFactory redisConnectionFactory() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master(redisProperties.getSentinel().getMaster());
+        redisProperties.getSentinel().getNodes().forEach(s -> sentinelConfig.sentinel(s, redisProperties.getPort()));
+        sentinelConfig.setPassword(RedisPassword.of(redisProperties.getPassword()));
 
-
-        return new LettuceConnectionFactory(configuration);
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .commandTimeout(redisCommandTimeout).readFrom(ReadFrom.REPLICA_PREFERRED).build();
+        return new LettuceConnectionFactory(sentinelConfig, clientConfig);
     }
 
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new GenericToStringSerializer<>(Object.class));
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 
+    @Override
     @Bean
     public RedisCacheManager cacheManager() {
         ObjectMapper mapper = new ObjectMapper();
 
 
-        RedisCacheConfiguration cacheConfig = myDefaultCacheConfig(Duration.ofMinutes(10), mapper).disableCachingNullValues();
+        RedisCacheConfiguration cacheConfig = cacheConfiguration(Duration.ofMinutes(10), mapper).disableCachingNullValues();
 
 
         return RedisCacheManager.builder(redisConnectionFactory())
                 .cacheDefaults(cacheConfig)
-                .withCacheConfiguration("carts_items", myDefaultCacheConfig(Duration.ofMinutes(5), mapper))
-                .withCacheConfiguration("carts_items", myDefaultCacheConfig(Duration.ofMinutes(1), mapper))
+                .withCacheConfiguration("carts_items", cacheConfiguration(Duration.ofMinutes(5), mapper))
+                .withCacheConfiguration("carts_items", cacheConfiguration(Duration.ofMinutes(1), mapper))
                 .build();
     }
 
 
-    private RedisCacheConfiguration myDefaultCacheConfig(Duration duration, ObjectMapper mapper) {
-
-
+    private RedisCacheConfiguration cacheConfiguration(Duration duration, ObjectMapper mapper) {
         var myMapper = mapper.copy ()
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .registerModule (
-                        new Hibernate6Module ()
+                        new Hibernate6Module()
                                 .enable(Hibernate6Module.Feature.FORCE_LAZY_LOADING)
                                 .enable(Hibernate6Module.Feature.REPLACE_PERSISTENT_COLLECTIONS)
                 ).activateDefaultTyping (
@@ -122,7 +129,5 @@ public class RedisConfig implements CachingConfigurer {
             }
         };
     }
-
-
 
 }
